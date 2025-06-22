edcbe01865ef435806ec45375633d4959198cd9c
nero31994 <arenaofvalorph937@gmail.com>
2025-06-22 17:55:51 +0800

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,109 +1,92 @@
-
-    const cardData = [
-      {
-        name: "ABC AUSTRALIA ",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397142/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/abc-australia.png?cache=",
-        "url": "go:ABC"
-          "category": "International"
-      },
-       {
-        "name": "ABN AUSTRALIA",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397140/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/3abn-international.png?c",
-        "url": "go:ABN"
-      },
-      {
-        "name": "A2Z",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397141/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/a2z.png?cache=20250322042023",
-        "url": "go:A2Z"
-      },
-      {
-        "name": "ALL JAZEERA ",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397143/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/al-jazeera.png?cache=202",
-        "url": "go:ALLJ"
-      },
-      {
-        "name": "ALLTV",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397146/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/alltv.png?cache=20250322042023",
-        "url": "go:ALLTV"
-      },
-       {
-        "name": "AL SAUDIYA ",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397145/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/al-saudiya.png?cache=2025",
-        "url": "go:ALS"
-      },
-      {
-        "name": "ALIW CHANNEL",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397145/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/aliw-channel-23.png?cach",
-        "url": "go:ALIW"
-      },
-      {
-        "name": "ANC",
-        "logo": "https://res.cloudinary.com/dra1xqho7/image/upload/v1738848867/IPTV/Converge%20Vision/anc.png?cache=20250322042023",
-        "url": "go:ANC"
-      },
-      {
-        "name": "ANIMAL PLANET",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397148/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/animal-planet.png?cache=",
-        "url": "go:ANIMAL"
-      },
-      {
-        "name": "ANIMAX",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397149/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/animax.png?cache=2025032",
-        "url": "go:ANIMAX"
-      },
-        {
-        "name": "ARIRANG",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397150/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/arirang.png?cache=20250322042023",
-        "url": "go:ARIRANG"
-      },
-        {
-        "name": "AFN",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397151/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/asian-food-network.png?cache=20250322042023",
-        "url": "go:AFN"
-      },
-        {
-        "name": "AXN",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397152/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/axn.png?cache=20250322042023",
-        "url": "go:AXN"
-      },
-         {
-        "name": "BBC NEWS",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397152/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/bbc-news.png?cache=20250322042023",
-        "url": "go:BBCN"
-      },
-        {
-        "name": "BILYONARYO",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397154/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/bilyonaryo-news-channel.png?cache=20250322042023",
-        "url": "go:BILYONARYO"
-      },
-        {
-        "name": "ANIMAX",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397149/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/animax.png?cache=2025032",
-        "url": "go:ANIMAX"
-      },
-        {
-        "name": "ANIMAX",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397149/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/animax.png?cache=2025032",
-        "url": "go:ANIMAX"
-      }, 
-        {
-        "name": "ANIMAX",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397149/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/animax.png?cache=2025032",
-        "url": "go:ANIMAX"
-      },
-        {
-        "name": "ANIMAX",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397149/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/animax.png?cache=2025032",
-        "url": "go:ANIMAX"
-      },
-        {
-        "name": "ANIMAX",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397149/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/animax.png?cache=2025032",
-        "url": "go:ANIMAX"
-      },
-        {
-        "name": "ANIMAX",
-        "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397149/KayLomiToWagNakawin/JokeLang/KungGagamitinLogoPaCreditNaLangAko/Salamat/IPTV/Converge%20Vision/animax.png?cache=2025032",
-        "url": "go:ANIMAX"
-        }
-    ];
+const cardData = [
+  {
+    name: "ABC AUSTRALIA",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397142/.../abc-australia.png?cache=",
+    url: "go:ABC",
+    category: "International"
+  },
+  {
+    name: "ABN AUSTRALIA",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397140/.../3abn-international.png?c",
+    url: "go:ABN",
+    category: "International"
+  },
+  {
+    name: "A2Z",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397141/.../a2z.png?cache=...",
+    url: "go:A2Z",
+    category: "Entertainment"
+  },
+  {
+    name: "ALL JAZEERA",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397143/.../al-jazeera.png?cache=202",
+    url: "go:ALLJ",
+    category: "News"
+  },
+  {
+    name: "ALLTV",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397146/.../alltv.png?cache=20250322042023",
+    url: "go:ALLTV",
+    category: "Entertainment"
+  },
+  {
+    name: "AL SAUDIYA",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397145/.../al-saudiya.png?cache=2025",
+    url: "go:ALS",
+    category: "International"
+  },
+  {
+    name: "ALIW CHANNEL",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397145/.../aliw-channel-23.png?cach",
+    url: "go:ALIW",
+    category: "Entertainment"
+  },
+  {
+    name: "ANC",
+    logo: "https://res.cloudinary.com/dra1xqho7/image/upload/v1738848867/.../anc.png?cache=20250322042023",
+    url: "go:ANC",
+    category: "News"
+  },
+  {
+    name: "ANIMAL PLANET",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397148/.../animal-planet.png?cache=",
+    url: "go:ANIMAL",
+    category: "Education"
+  },
+  {
+    name: "ANIMAX",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397149/.../animax.png?cache=2025032",
+    url: "go:ANIMAX",
+    category: "Anime"
+  },
+  {
+    name: "ARIRANG",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397150/.../arirang.png?cache=20250322042023",
+    url: "go:ARIRANG",
+    category: "International"
+  },
+  {
+    name: "AFN",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397151/.../asian-food-network.png?cache=20250322042023",
+    url: "go:AFN",
+    category: "Education"
+  },
+  {
+    name: "AXN",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397152/.../axn.png?cache=20250322042023",
+    url: "go:AXN",
+    category: "Entertainment"
+  },
+  {
+    name: "BBC NEWS",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397152/.../bbc-news.png?cache=20250322042023",
+    url: "go:BBCN",
+    category: "News"
+  },
+  {
+    name: "BILYONARYO",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397154/.../bilyonaryo-news-channel.png?cache=20250322042023",
+    url: "go:BILYONARYO",
+    category: "News"
+  }
+];
