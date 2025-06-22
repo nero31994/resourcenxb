b8784d5344c1271b41ffb19ed07d9446b0b1e484
nero31994 <arenaofvalorph937@gmail.com>
2025-06-22 17:45:51 +0800

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,92 +1,92 @@
 const cardData = [
   {
-    "name": "ABC AUSTRALIA",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397142/.../abc-australia.png?cache=",
-    "url": "go:ABC",
-    "category": "International"
+    name: "ABC AUSTRALIA",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397142/.../abc-australia.png?cache=",
+    url: "go:ABC",
+    category: "International"
   },
   {
-    "name": "ABN AUSTRALIA",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397140/.../3abn-international.png?c",
-    "url": "go:ABN",
-    "category": "International"
+    name: "ABN AUSTRALIA",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397140/.../3abn-international.png?c",
+    url: "go:ABN",
+    category: "International"
   },
   {
-    "name": "A2Z",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397141/.../a2z.png?cache=...",
-    "url": "go:A2Z",
-    "category": "Entertainment"
+    name: "A2Z",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397141/.../a2z.png?cache=...",
+    url: "go:A2Z",
+    category: "Entertainment"
   },
   {
-    "name": "ANIMAX",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397149/.../animax.png?cache=2025032",
-    "url": "go:ANIMAX",
-    "category": "Anime"
+    name: "ALL JAZEERA",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397143/.../al-jazeera.png?cache=202",
+    url: "go:ALLJ",
+    category: "News"
   },
   {
-    "name": "ALL JAZEERA",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397143/.../al-jazeera.png?cache=202",
-    "url": "go:ALLJ",
-    "category": "News"
+    name: "ALLTV",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397146/.../alltv.png?cache=20250322042023",
+    url: "go:ALLTV",
+    category: "Entertainment"
   },
   {
-    "name": "ALLTV",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397146/.../alltv.png?cache=20250322042023",
-    "url": "go:ALLTV",
-    "category": "Entertainment"
+    name: "AL SAUDIYA",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397145/.../al-saudiya.png?cache=2025",
+    url: "go:ALS",
+    category: "International"
   },
   {
-    "name": "AL SAUDIYA",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397145/.../al-saudiya.png?cache=2025",
-    "url": "go:ALS",
-    "category": "International"
+    name: "ALIW CHANNEL",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397145/.../aliw-channel-23.png?cach",
+    url: "go:ALIW",
+    category: "Entertainment"
   },
   {
-    "name": "ALIW CHANNEL",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397145/.../aliw-channel-23.png?cach",
-    "url": "go:ALIW",
-    "category": "Entertainment"
+    name: "ANC",
+    logo: "https://res.cloudinary.com/dra1xqho7/image/upload/v1738848867/.../anc.png?cache=20250322042023",
+    url: "go:ANC",
+    category: "News"
   },
   {
-    "name": "ANC",
-    "logo": "https://res.cloudinary.com/dra1xqho7/image/upload/v1738848867/.../anc.png?cache=20250322042023",
-    "url": "go:ANC",
-    "category": "News"
+    name: "ANIMAL PLANET",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397148/.../animal-planet.png?cache=",
+    url: "go:ANIMAL",
+    category: "Education"
   },
   {
-    "name": "ANIMAL PLANET",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397148/.../animal-planet.png?cache=",
-    "url": "go:ANIMAL",
-    "category": "Education"
+    name: "ANIMAX",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397149/.../animax.png?cache=2025032",
+    url: "go:ANIMAX",
+    category: "Anime"
   },
   {
-    "name": "ARIRANG",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397150/.../arirang.png?cache=20250322042023",
-    "url": "go:ARIRANG",
-    "category": "International"
+    name: "ARIRANG",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397150/.../arirang.png?cache=20250322042023",
+    url: "go:ARIRANG",
+    category: "International"
   },
   {
-    "name": "AFN",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397151/.../asian-food-network.png?cache=20250322042023",
-    "url": "go:AFN",
-    "category": "Education"
+    name: "AFN",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397151/.../asian-food-network.png?cache=20250322042023",
+    url: "go:AFN",
+    category: "Education"
   },
   {
-    "name": "AXN",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397152/.../axn.png?cache=20250322042023",
-    "url": "go:AXN",
-    "category": "Entertainment"
+    name: "AXN",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397152/.../axn.png?cache=20250322042023",
+    url: "go:AXN",
+    category: "Entertainment"
   },
   {
-    "name": "BBC NEWS",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397152/.../bbc-news.png?cache=20250322042023",
-    "url": "go:BBCN",
-    "category": "News"
+    name: "BBC NEWS",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397152/.../bbc-news.png?cache=20250322042023",
+    url: "go:BBCN",
+    category: "News"
   },
   {
-    "name": "BILYONARYO",
-    "logo": "https://res.cloudinary.com/lomi/image/upload/v1742397154/.../bilyonaryo-news-channel.png?cache=20250322042023",
-    "url": "go:BILYONARYO",
-    "category": "News"
+    name: "BILYONARYO",
+    logo: "https://res.cloudinary.com/lomi/image/upload/v1742397154/.../bilyonaryo-news-channel.png?cache=20250322042023",
+    url: "go:BILYONARYO",
+    category: "News"
   }
 ];
