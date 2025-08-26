59d932a13ef9b8067b8879372ce8e16f9d455d61
nero31994 <arenaofvalorph937@gmail.com>
2025-08-26 10:50:00 +0800

    Update Card.js

--- Card.js
+++ Card.js
@@ -2,7 +2,7 @@
     const CARDS = [
         { title: "Follow my Page", href: "go:FB", image: "https://media.giphy.com/headers/facebook/FuLFvah0klRm.gif", desc: "Follow my Page for Updates", badge: "Follow" },
         { title: "Donate with ❤️", href: "go:GC", image: "https://i.giphy.com/lnfPlSN43yzFDQCVry.webp", desc: "You Contribution Help me to Continue Building Iptv App", badge: "Donate" },
-        { title: "EPG", href: "go:EPG", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJZUwof3mnIZJ5CHLcREuYYwudyW-9cbLD69wWqg7u3kdzX_hafWNUSs_m&s=10", desc: "Electronic Program Guide", badge: "EPG" },
+        { title: "EPG", href: "go:EPG", image: "https://res.cloudinary.com/dglljk9gs/image/upload/v1756176528/file_0000000095e861fa8e42c68a790bb0c3_jqgqjv.png", desc: "Electronic Program Guide", badge: "EPG" },
     
           { title: "Kapamilya Channel", href: "go:1", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJm6yEX2gKUoTEn-wvvXwWZaJ3Vp84BRaz3fKDLZbx6fEl98tBiS2TG7-i&s=10", desc: "ENTERTAINMENT", badge: "LIVE" },
           { title: "Kapamilya Channel HD", href: "go:2", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGFbFbOhLNpCNEkO02nb2f5zzwFveMuxhVVIbjWEoVdIWayrYk8ShRWsGB&s=10", desc: "ENTERTAINMENT", badge: "HD" },
